140b97a629da4e84d0979ed1ddc44fdc4536a78d
Michael Ebneth <michael.ebneth@wisit.com>
2019-06-12 19:37:42 +0200

    Stable

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,13 +1,30 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton imageButton6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.tianyi);
+        setContentView(R.layout.activity_main);
+
+        imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
+/*
+        imageButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentLocalNewActivty = new Intent(MainActivity.this, NewActivity.class);
+                startActivity(intentLoadNewActivity);
+            }
+        });
+*/
     }
 }
