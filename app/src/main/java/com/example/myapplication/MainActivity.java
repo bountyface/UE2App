b9af3a1dd425cf243d66e3ee0b0f157c28363b50
Michael Ebneth <michael.ebneth@tu-ilmenau.de>
2019-06-23 16:09:16 +0200

    (MainMenu) Added functionality to switch to another activity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,30 +1,42 @@
 package com.example.myapplication;
 
+
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton imageButton6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //imageButton6 = (ImageButton) findViewById(R.id.imageButton6);
-/*
-        imageButton6.setOnClickListener(new View.OnClickListener() {
+        // for testing purpose
+        /*
+        ImageView kitchenImageView = (ImageView) findViewById(R.id.MM_Kitchen_ImageView);
+        kitchenImageView.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View v) {
-                Intent intentLocalNewActivty = new Intent(MainActivity.this, NewActivity.class);
-                startActivity(intentLoadNewActivity);
+            public void onClick(View v){
+                displayToast("Kitchen");
+                Intent testIntent = new Intent(getApplicationContext(), test.class);
+                startActivity(testIntent);
             }
         });
-*/
+        */
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    public void onKitchenImageViewClick(View view) {
+        displayToast("Kitchen");
+        Intent testIntent = new Intent(getApplicationContext(), test.class);
+        startActivity(testIntent);
     }
 }
