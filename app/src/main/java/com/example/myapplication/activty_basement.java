1d75b5cae6fabc094a8399f0ab12e063740d7b28
ffff <tianyi.liu@tu-ilmenau.de>
2019-06-24 12:49:45 +0200

    basement activty

---
+++ app/src/main/java/com/example/myapplication/activty_basement.java
@@ -0,0 +1,13 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class activty_basement extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activty_basement);
+    }
+}
