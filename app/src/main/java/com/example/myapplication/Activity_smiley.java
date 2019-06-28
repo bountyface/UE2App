77a9c8ad80885218409f89ec762bd068ee43c032
Michael Ebneth <michael.ebneth@wisit.com>
2019-06-28 16:28:29 +0200

    (Kitchen, Smiley) Value exchange in between working

--- app/src/main/java/com/example/myapplication/Activity_smiley.java
+++ app/src/main/java/com/example/myapplication/Activity_smiley.java
@@ -1,22 +1,48 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Activity_smiley extends AppCompatActivity {
-
+public Intent receivedIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_smiley);
+
+        //Intent intent = getIntent();
+        receivedIntent = getIntent();
+        int id = receivedIntent.getIntExtra("viewId",0);
+        //int number1 = intent.getIntExtra("number1", 0);
     }
 
-    public void onSmileyGreenClick(){
+    public void onSmileyGreenClick(View view){
+        Log.d("mytag",""+view.getId());
+
+
+        Intent intent = new Intent();
+        intent.putExtra("color","#D81B60" );
+        intent.putExtra("id", receivedIntent.getIntExtra("viewId",0));
+        setResult(RESULT_OK, intent);
+
+        finish();
+        //ImageView imgView = (ImageView) findViewById(view.getId());
+        //imgView.setBackgroundColor(Color.parseColor("#D81B60"));
+
+
+        //view.getId();
+
 
     }
-    public void onSmileyYellowClick(){
+    public void onSmileyYellowClick(View view){
 
-    }public void onSmileyRedClick(){
+    }
+    public void onSmileyRedClick(View view){
 
     }
 }
--- app/src/main/java/com/example/myapplication/activity_kitchen.java
+++ app/src/main/java/com/example/myapplication/activity_kitchen.java
@@ -2,10 +2,13 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class Activity_kitchen extends AppCompatActivity {
 
@@ -21,9 +24,29 @@ public class Activity_kitchen extends AppCompatActivity {
     }
 
     public void onSubmenuTileClick(View view){
+        int x = 21;
         Log.d("mytag","onSubmenuTile Clicked!!!!!!");
-        Intent testIntent = new Intent(this, Activity_smiley.class);
-        startActivity(testIntent);
 
+        Intent intent = new Intent(this, Activity_smiley.class);
+        intent.putExtra("viewId",view.getId());
+        startActivityForResult(intent,1);
+        //startActivity(testIntent);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==1){
+            if(resultCode==RESULT_OK){
+                String color = data.getStringExtra("color");
+                ImageView imgView = (ImageView) findViewById(data.getIntExtra("id",0));
+                imgView.setBackgroundColor(Color.parseColor("#D81B60"));
+                Log.d("mytag","RESULT_OK: "+color);
+            }
+            if(resultCode==RESULT_CANCELED){
+                Log.d("mytag","RESULT_CANCELED");
+            }
+        }
     }
 }
