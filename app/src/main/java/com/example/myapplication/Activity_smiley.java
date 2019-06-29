e5d4167c3453e04f1efb6046c3596fd10f3f44f4
Michael Ebneth <michael.ebneth@wisit.com>
2019-06-29 21:59:34 +0200

    (Smiley) Audio Info implemented

--- app/src/main/java/com/example/myapplication/Activity_smiley.java
+++ app/src/main/java/com/example/myapplication/Activity_smiley.java
@@ -2,15 +2,23 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class Activity_smiley extends AppCompatActivity {
-public Intent receivedIntent;
+
+    ImageView assistant;
+    boolean playAudio = true;
+
+    public Intent receivedIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,34 +26,71 @@ public Intent receivedIntent;
 
         //Intent intent = getIntent();
         receivedIntent = getIntent();
-        int id = receivedIntent.getIntExtra("viewId",0);
+        int id = receivedIntent.getIntExtra("viewId", 0);
         //int number1 = intent.getIntExtra("number1", 0);
+
+        final MediaPlayer smiley_info = MediaPlayer.create(this, R.raw.smiley_info);
+        smiley_info.start();
+        animateAssistant();
     }
 
-    public void onSmileyGreenClick(View view){
+    public void onSmileyGreenClick(View view) {
         Intent intent = new Intent();
         intent.putExtra("color", R.color.colorTaskPositive);
-        intent.putExtra("id", receivedIntent.getIntExtra("viewId",0));
+        intent.putExtra("id", receivedIntent.getIntExtra("viewId", 0));
         setResult(RESULT_OK, intent);
 
         finish();
 
     }
-    public void onSmileyYellowClick(View view){
+
+    public void onSmileyYellowClick(View view) {
         Intent intent = new Intent();
-        intent.putExtra("color",R.color.colorTaskNeutral );
-        intent.putExtra("id", receivedIntent.getIntExtra("viewId",0));
+        intent.putExtra("color", R.color.colorTaskNeutral);
+        intent.putExtra("id", receivedIntent.getIntExtra("viewId", 0));
         setResult(RESULT_OK, intent);
 
         finish();
 
     }
-    public void onSmileyRedClick(View view){
+
+    public void onSmileyRedClick(View view) {
         Intent intent = new Intent();
-        intent.putExtra("color",R.color.colorTaskNegative );
-        intent.putExtra("id", receivedIntent.getIntExtra("viewId",0));
+        intent.putExtra("color", R.color.colorTaskNegative);
+        intent.putExtra("id", receivedIntent.getIntExtra("viewId", 0));
         setResult(RESULT_OK, intent);
 
         finish();
+    }
+
+    public void onSupervisorClick(View view) {
+        Intent intent = new Intent(this, Activity_skype.class);
+        startActivityForResult(intent, 1);
+    }
+
+    public void onAssistantClick(View view) {
+        animateAssistant();
+        final MediaPlayer smiley_info = MediaPlayer.create(this, R.raw.smiley_info);
+
+
+        if (playAudio) {
+            smiley_info.start();
+        }
+
+        playAudio = false;
+        smiley_info.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                //animateAssistant();
+                playAudio = true;
+            }
+        });
+    }
+
+    private void animateAssistant() {
+        assistant = (ImageView) findViewById(R.id.assistant_ImageView);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.shake1);
+        assistant.startAnimation(animation);
     }
 }
--- app/src/main/java/com/example/myapplication/activity_kitchen.java
+++ app/src/main/java/com/example/myapplication/activity_kitchen.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
