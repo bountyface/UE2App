f406adc76c7431adac872108fbd4892ab14f29d7
Michael Ebneth <michael.ebneth@wisit.com>
2019-06-30 01:36:17 +0200

    (Bathroom) Applied changes

--- app/src/main/java/com/example/myapplication/Activity_bathroom.java
+++ app/src/main/java/com/example/myapplication/Activity_bathroom.java
@@ -1,34 +1,47 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 public class Activity_bathroom extends AppCompatActivity {
+
+    ImageView assistant;
+    public int color;
+
+    boolean playAudio = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bathroom);
-        Log.d("mytag","dining created");
+        Log.d("mytag", "dining created");
+
+        final MediaPlayer sm_task = MediaPlayer.create(this, R.raw.sm_task);
+        sm_task.start();
+        animateAssistant();
 
     }
-    public void onBackButtonClick(View view){
+
+    public void onBackButtonClick(View view) {
         Intent testIntent = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(testIntent);
     }
 
-    public void onSubmenuTileClick(View view){
+    public void onSubmenuTileClick(View view) {
         int x = 21;
-        Log.d("mytag","onSubmenuTile Clicked!!!!!!");
+        Log.d("mytag", "onSubmenuTile Clicked!!!!!!");
 
         Intent intent = new Intent(this, Activity_smiley.class);
-        intent.putExtra("viewId",view.getId());
-        startActivityForResult(intent,1);
+        intent.putExtra("viewId", view.getId());
+        startActivityForResult(intent, 1);
         //startActivity(testIntent);
     }
 
@@ -36,20 +49,107 @@ public class Activity_bathroom extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode==1){
-            if(resultCode==RESULT_OK){
-                int color = data.getIntExtra("color",0);
-                ImageView imgView = (ImageView) findViewById(data.getIntExtra("id",0));
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                color = data.getIntExtra("color", 0);
+                ImageView imgView = (ImageView) findViewById(data.getIntExtra("id", 0));
                 imgView.setBackgroundResource(color);
-                Log.d("mytag","RESULT_OK: "+color);
+
+                final MediaPlayer task_whenfinished = MediaPlayer.create(this, R.raw.task_whenfinished);
+
+                String result = data.getStringExtra("result");
+                switch (result) {
+                    case "green":
+                        final MediaPlayer smiley_feedback_green = MediaPlayer.create(this, R.raw.smiley_feedback_green);
+
+                        if (playAudio) smiley_feedback_green.start();
+                        playAudio = false;
+                        smiley_feedback_green.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                            @Override
+                            public void onCompletion(MediaPlayer mp) {
+                                animateAssistant();
+                                task_whenfinished.start();
+                                playAudio = true;
+                            }
+                        });
+
+                        animateAssistant();
+                        break;
+                    case "yellow":
+                        final MediaPlayer smiley_feedback_yellow = MediaPlayer.create(this, R.raw.smiley_feedback_yellow);
+                        if (playAudio) smiley_feedback_yellow.start();
+                        playAudio = false;
+                        smiley_feedback_yellow.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                            @Override
+                            public void onCompletion(MediaPlayer mp) {
+                                animateAssistant();
+                                task_whenfinished.start();
+                                playAudio = true;
+                            }
+                        });
+                        animateAssistant();
+                        break;
+                    case "red":
+                        final MediaPlayer smiley_feedback_red = MediaPlayer.create(this, R.raw.smiley_feedback_red);
+                        if (playAudio) smiley_feedback_red.start();
+                        playAudio = false;
+                        smiley_feedback_red.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                            @Override
+                            public void onCompletion(MediaPlayer mp) {
+                                animateAssistant();
+                                task_whenfinished.start();
+                                playAudio = true;
+                            }
+                        });
+                        animateAssistant();
+                        break;
+                    default:
+                        break;
+                }
+
+                Log.d("mytag", "RESULT_OK: " + color);
             }
-            if(resultCode==RESULT_CANCELED){
-                Log.d("mytag","RESULT_CANCELED");
+            if (resultCode == RESULT_CANCELED) {
+                Log.d("mytag", "RESULT_CANCELED");
             }
         }
     }
-    public void onSupervisorClick(View view){
+
+
+    public void onSupervisorClick(View view) {
         Intent intent = new Intent(this, Activity_skype.class);
-        startActivityForResult(intent,1);
+        startActivityForResult(intent, 1);
     }
+
+    public void onAssistantClick(View view) {
+        final MediaPlayer sm_state_bathroom = MediaPlayer.create(this, R.raw.sm_state_bathroom);
+        final MediaPlayer sm_task = MediaPlayer.create(this, R.raw.sm_task);
+
+        if (playAudio) {
+            sm_state_bathroom.start();
+        }
+
+        playAudio = false;
+
+        sm_state_bathroom.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                animateAssistant();
+                sm_task.start();
+                playAudio = true;
+            }
+        });
+        animateAssistant();
+    }
+
+    private void animateAssistant() {
+        assistant = (ImageView) findViewById(R.id.assistant_ImageView);
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.shake1);
+        assistant.startAnimation(animation);
+    }
+
 }
